1e3fde9ee7571e739a16fc56e3bb5114d7f28e6e
Dianfer <939978298@qq.com>
2019-01-01 14:10:09 +0800

    注释

--- module/mod-event/external-threat/index.vue
+++ module/mod-event/external-threat/index.vue
@@ -1,5 +1,14 @@
 <template>
     <section>
+        <!-- 什么注释都行
+        1
+        1
+        2
+        
+        3
+        3
+        4
+        4 -->
         <router-view></router-view>
     </section>
 </template>
